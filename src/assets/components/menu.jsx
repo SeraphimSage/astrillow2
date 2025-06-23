a5bb971d1387f08d73d1b538c7a84e19f8cb731e
Kenneth Pinkerton <kenneth.pinkerton90@gmail.com>
2025-06-23 09:43:39 -0500

    Updated logo to  properly reflect animation with CSS.

--- src/assets/components/menu.jsx
+++ src/assets/components/menu.jsx
@@ -3,22 +3,26 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import PersonalLogo from "../images/Favicon.png";
 
+import "../css/menu.css";
+
 function Menu() {
 	return (
 		<Navbar
 			expand="lg"
 			className="bg-body-tertiary"
 		>
-			<Container>
+			<Container className="navbar-wrapper">
 				<Navbar.Brand href="/">
-					<img
-						alt="Personal Logo"
-						src={PersonalLogo}
-						width="30"
-						height="30"
-						className="d-inline-block align-top"
-					/>{" "}
-					Astrillow2
+					<div className="logo-content-wrapper">
+						<img
+							alt="Personal Logo"
+							src={PersonalLogo}
+							width="30"
+							height="30"
+							className="logo d-inline-block align-top"
+						/>{" "}
+						Astrillow2
+					</div>
 				</Navbar.Brand>
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
