7c61676858f2638124a81f855a82645389d3d920
Kenneth Pinkerton <kenneth.pinkerton90@gmail.com>
2025-07-07 11:01:30 -0500

    Refactor Profile component to include Menu and Footer; enhance user display with conditional rendering based on authentication status.

--- src/assets/components/profile.jsx
+++ src/assets/components/profile.jsx
@@ -1,4 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Menu from "./menu.jsx";
+import Footer from "./footer.jsx";
 
 const Profile = () => {
 	const { user, isAuthenticated, isLoading } = useAuth0();
@@ -7,17 +9,25 @@ const Profile = () => {
 		return <div>Loading ...</div>;
 	}
 
-	return (
-		isAuthenticated && (
-			<div>
-				<h2>{user.name}</h2>
-				<img
-					src={user.picture}
-					alt={user.name}
-				/>
-				<p>{user.email}</p>
-			</div>
-		)
+	return isAuthenticated ? (
+		<div>
+			<Menu />
+			<h2 className="text-white text-lg p-4 m-4">{user.name}</h2>
+			<img
+				src={user.picture}
+				alt={user.name}
+			/>
+			<p className="text-white text-lg p-4 m-4">{user.email}</p>
+			<Footer />
+		</div>
+	) : (
+		<div>
+			<Menu />
+			<h2 className="w-fit bg-dark-space text-white text-2xl font-bold p-4 m-4 rounded-lg shadow-neon-glow">
+				Please log in to view your profile
+			</h2>
+			<Footer />
+		</div>
 	);
 };
 
